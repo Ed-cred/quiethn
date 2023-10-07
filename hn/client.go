70b339bf3e8e267fe73302a727beb796375b136a
Ed-cred <edwardandrew63@gmail.com>
2023-10-07 16:06:28 +0300

    project scaffold and client setup

---
+++ hn/client.go
@@ -0,0 +1,67 @@
+package hn
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const (
+	apiBase = "https://hacker-news.firebaseio.com/v0"
+)
+
+type Client struct {
+	apiBase string
+}
+
+func (c *Client) defaultBase() {
+	if c.apiBase == "" {
+		c.apiBase = apiBase
+	}
+}
+
+func (c *Client) TopItems() ([]int, error) {
+	c.defaultBase()
+	resp, err := http.Get(fmt.Sprintf("%s/topstories.json", c.apiBase))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var ids []int
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&ids)
+	if err != nil {
+		return nil, err
+	}
+	return ids, nil
+}
+
+func (c *Client) GetItem(id int) (Item, error) {
+	c.defaultBase()
+	var item Item
+	resp, err := http.Get(fmt.Sprintf("%s/item/%d.json", c.apiBase, id))
+	if err != nil {
+		return item, err
+	}
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&item)
+	if err != nil {
+		return item, err
+	}
+	return item, nil
+}
+
+type Item struct {
+	ID          int    `json:"id"`
+	By          string `json:"by"`
+	Descendants int    `json:"descendants"`
+	Kids        []int  `json:"kids"`
+	Score       int    `json:"score"`
+	Time        int    `json:"time"`
+	Title       string `json:"title"`
+	Type        string `json:"type"`
+	// These cannot exist at the same time
+	Text string `json:"text"`
+	URL  string `json:"url"`
+}
